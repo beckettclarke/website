2571290577893c877a5ec495effc5be9006347f3
Beckett Clarke <91700492+ItsFoxDev@users.noreply.github.com>
2024-08-14 18:28:29 -0400

    🎉 Big commit, more coming soon

---
+++ globalfuncs.js
@@ -0,0 +1,6 @@
+function switchtab(e){
+  console.log(e);
+}
+String.prototype.toProperCase = function () {
+  return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+};
